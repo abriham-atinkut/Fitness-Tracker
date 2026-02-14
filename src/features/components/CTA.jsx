8ecc94d4c1c27257a9d2bcd5473ed34e6c16dbab
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-02-14 22:06:18 +0300

    feat: add CTA; improve tailwind directives for secondary button

---
+++ src/features/components/CTA.jsx
@@ -0,0 +1,21 @@
+const CTA = () => {
+  return (
+    <>
+      <div className="py-20 px-24 bg-slate-50 text-center">
+        <div className="mb-10">
+          <h1 className="mb-4">A Smarter Way to Track Your Fitness Progress</h1>
+          <p>
+            Designed with clean analytics, structured workouts, and a simple
+            interface to help you stay consistent and focused.
+          </p>
+        </div>
+        <div>
+          <button className="btn-primary m-4">Get Started Free</button>
+          <button className="btn-secondary">View Project On GitHub</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CTA;
