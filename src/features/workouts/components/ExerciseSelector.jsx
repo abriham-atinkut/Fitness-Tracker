9ccf1248492cc0a8e04061f55b8d7cbe61134e86
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-02-20 00:16:27 +0300

    feat(exercise selector): add search filtering for workout and display 4 result based on search word

--- src/features/workouts/components/ExerciseSelector.jsx
+++ src/features/workouts/components/ExerciseSelector.jsx
@@ -1,17 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import fetchExercise from "../api/exercisesApi";
 
 const ExerciseSelector = () => {
-  console.log("first");
+  const [search, setSearch] = useState("");
+  const [workouts, setWorkouts] = useState([]);
+
   useEffect(() => {
-    const logeData = async () => {
+    // fetch exercises when component mount
+    const loadExercise = async () => {
       const data = await fetchExercise();
-      console.log("useEffect clg");
-      console.log(data);
+      setWorkouts(data);
     };
-    logeData();
+    loadExercise();
   }, []);
-  return <div>ExerciseSelector</div>;
+
+  let filteredWorkouts = workouts.filter((workout) => {
+    return workout.toLowerCase().includes(search.toLowerCase());
+  });
+
+  let visibleWorkout = filteredWorkouts.slice(0, 4);
+
+  return (
+    <>
+      <div>
+        <div>
+          <input
+            type="text"
+            className="border-2 outline-none rounded-md"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+
+        <div>
+          {visibleWorkout?.map((workout, i) => (
+            <div key={i}>{workout}</div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ExerciseSelector;
