a03fed0af5bbc5a03b9e86736cf8b36031d1eb7d
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-02-19 19:56:33 +0300

    feat(api): fetch API from wger and return only list of exercises

--- src/features/workouts/components/ExerciseSelector.jsx
+++ src/features/workouts/components/ExerciseSelector.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { useEffect } from "react";
+import fetchExercise from "../api/exercisesApi";
 
 const ExerciseSelector = () => {
-  return (
-    <div>ExerciseSelector</div>
-  )
-}
+  console.log("first");
+  useEffect(() => {
+    const logeData = async () => {
+      const data = await fetchExercise();
+      console.log("useEffect clg");
+      console.log(data);
+    };
+    logeData();
+  }, []);
+  return <div>ExerciseSelector</div>;
+};
 
-export default ExerciseSelector
+export default ExerciseSelector;
