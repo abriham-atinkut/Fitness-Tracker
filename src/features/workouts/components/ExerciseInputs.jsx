ac6cd8bde7cca05d1791d9e9f5ce6d33892ebe62
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-02-21 21:27:52 +0300

    Revert "feat(Exercise-Inputs): implement workout form with form handler; add basic styling using tailwind."
    This reverts commit 77e7edc99dc9cbfe2c37c392b48694dbfcfe0d18.

--- src/features/workouts/components/ExerciseInputs.jsx
+++ src/features/workouts/components/ExerciseInputs.jsx
@@ -15,7 +15,7 @@ const ExerciseInputs = () => {
 
   return (
     <>
-      <form onSubmit={formHandler}>
+      <form onSubmit={formHandled}>
         <input
           type="text"
           className="border-2 w-full rounded-md mb-2"
