df2060aef1c1c2e12ac67aa04ef9a3e429163335
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-02-24 22:35:58 +0300

    feat(AddedExercisesList): render submitted workouts from zustand store

--- src/features/workouts/components/AddedExercisesList.jsx
+++ src/features/workouts/components/AddedExercisesList.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import useWorkout from "../hooks/useWorkouts";
 
 const AddedExercisesList = () => {
+  const workouts = useWorkout((state) => state.workouts);
   return (
-    <div>AddedExercisesList</div>
-  )
-}
+    <>
+      <div className="mt-8 text-center bg-slate-200 rounded-lg p-2">
+        {workouts?.map((workout) => (
+          <div key={workout.id} className="bg-slate-300 m-2">
+            <ul>
+              <li className=" space-x-4">
+                <span>{workout.sets}</span>
+                <span>{workout.reps}</span>
+                <span>{workout.weight}</span>
+              </li>
+            </ul>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default AddedExercisesList
+export default AddedExercisesList;
