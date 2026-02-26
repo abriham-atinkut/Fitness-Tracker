3f17efcbe72444cb1345523627407821030b2fd7
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-02-26 13:25:49 +0300

    style: enable vertical scrolling for exercises list

--- src/features/workouts/components/AddedExercisesList.jsx
+++ src/features/workouts/components/AddedExercisesList.jsx
@@ -7,20 +7,21 @@ const AddedExercisesList = () => {
     // when saved i need to connect to DashBoard state and that code goes in here
     clearWorkout();
   };
-
+  const reversedWorkouts = [...workouts].reverse();
+  console.log(reversedWorkouts)
   return (
     <div>
-      <div className="mt-4 text-center rounded-lg p-2 bg-zinc-100 dark:bg-zinc-700 ">
-        {workouts?.map((workout) => (
+      <div className="mt-4 text-center rounded-lg p-2 bg-zinc-100 dark:bg-zinc-700 h-28 overflow-y-auto">
+        {reversedWorkouts?.map((workout) => (
           <div
             key={workout.id}
             className="mb-1 p-1 rounded-md bg-zinc-200 hover:bg-zinc-300 dark:bg-zinc-800"
-          >   
+          >
             <ul>
               <li className="grid grid-rows-1 grid-cols-4">
                 <p>{workout.selectedWorkout}</p>
                 <p>
-                  {workout.sets}x{workout.reps}
+                  {workout.sets} x {workout.reps}
                 </p>
                 <p>{workout.weight} kg</p>
                 <button>Delete</button>
