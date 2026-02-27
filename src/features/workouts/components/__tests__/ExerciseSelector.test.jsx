2f0899c5e51027a1f76fc12bcb517e80d3f04456
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-02-27 16:47:11 +0300

    test(ExerciseSelector): mock API and zustand hook to test fetched exerciese rendering

--- src/features/workouts/components/__tests__/ExerciseSelector.test.jsx
+++ src/features/workouts/components/__tests__/ExerciseSelector.test.jsx
@@ -1,6 +1,32 @@
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import ExerciseSelector from "../ExerciseSelector";
+import useWorkout from "../../hooks/useWorkouts";
+import fetchExercise from "../../api/exercisesApi";
 
 test("render without crashing", () => {
   render(<ExerciseSelector />);
+});
+
+jest.mock("../../hooks/useWorkouts");
+jest.mock("../../api/exercisesApi");
+
+describe("ExercieseSelector", () => {
+  const mockUpdateSelectoryWorkout = jest.fn();
+  // console.log(mockUpdateSelectoryWorkout)
+  beforeEach(() => {
+    fetchExercise.mockResolvedValue([
+      "Push Up",
+      "Pull Up",
+      "Squat",
+      "Bench Press",
+    ]);
+    useWorkout.mockReturnValue(mockUpdateSelectoryWorkout);
+  });
+
+  test("render fetched exercises", async () => {
+    render(<ExerciseSelector />);
+    await waitFor(() => {
+      expect(screen.getByText("Push Up")).toBeInTheDocument();
+    });
+  });
 });
